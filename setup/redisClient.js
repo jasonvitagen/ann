aa4b13fc0c12904855952a1db45aaa451b0e21ad
Jason Cheng <qishen.cheng@ivisgroup.com>
2014-09-05 18:05:37 +0800

    COMMIT 1

---
+++ setup/redisClient.js
@@ -0,0 +1,21 @@
+function setup (redisClient) {
+
+	redisClient.on('error', function (err) {
+		console.log('Error: ' + err);
+	});
+	redisClient.on('ready', function () {
+		console.log('Redis client is ready');
+	});
+	redisClient.on('end', function () {
+		console.log('Redis has been ended');
+	});
+	redisClient.on('idle', function () {
+		console.log('Redis is idle');
+	});
+	
+}
+
+
+module.exports = {
+	setup : setup
+};
