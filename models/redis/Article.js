4fc58a955bed8d6042a152c66b05077e0200162f
Jason Cheng <qishen.cheng@ivisgroup.com>
2014-09-12 18:08:23 +0800

    COMMIT 6

---
+++ models/redis/Article.js
@@ -0,0 +1,15 @@
+var c;
+function Article (client) {
+	var client = client;
+}
+Article.prototype.save = function (article) {
+
+}
+
+function setup (client) {
+	c = client;
+}
+
+module.exports = {
+	setup : setup
+};
