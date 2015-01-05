445dcc5ec517db2d4b6bc3b5be7c8813fa926c78
Jason Cheng <jasoncheng@live.com.my>
2015-01-05 23:53:20 +0800

    COMMIT 75

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Article = require('../models/redis/Article').Article;
 var authMiddlewares = require('./middlewares/auth');
+var CrawledArticleModel = require('../models/mongo/CrawledArticle');
+var articleRoutesBehaviors = require('./articleRoutesBehaviors');
 
 
 router.get('/pending-confirmation-articles', authMiddlewares.isLoggedIn, authMiddlewares.isAdmin, function (req, res) {
@@ -21,6 +23,32 @@ router.post('/confirm-article', authMiddlewares.isLoggedIn, authMiddlewares.isAd
 	});
 });
 
+router.get('/list-crawled-articles', authMiddlewares.isLoggedIn, authMiddlewares.isAdmin, function (req, res) {
+	CrawledArticleModel
+		.find()
+		.sort({ created : -1 })
+		.select({ title : 1, _id : 1 })
+		.exec(function (err, crawledArticles) {
+			console.log(crawledArticles);
+			res.render('admin/list-crawled-articles', { crawledArticles : crawledArticles });
+		});
+});
 
+router.get('/list-crawled-article/:id', authMiddlewares.isLoggedIn, authMiddlewares.isAdmin, function (req, res) {
+	CrawledArticleModel
+		.find()
+		.where({ _id : req.params.id })
+		.exec(function (err, crawledArticle) {
+			crawledArticle = crawledArticle[0];
+			req.body.title = crawledArticle.title;
+			req.body.thumbnail = crawledArticle.thumbnail;
+			req.body.content = crawledArticle.content;
+			articleRoutesBehaviors.get.create.v1(req, res);
+		});
+});
+
+router.post('/list-crawled-article/:id', authMiddlewares.isLoggedIn, authMiddlewares.isAdmin, function (req, res) {
+	articleRoutesBehaviors.post.create.v2(req, res);
+});
 
 module.exports = router;
