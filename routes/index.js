8dc26aafc188bb54c77705dbc8316e91157c933e
Jason Cheng <jasoncheng@live.com.my>
2015-04-21 21:51:33 +0800

    COMMIT 115

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,20 @@ var express = require('express')
     , webFrontIndexConfig = require('../config/webfront/index')
     , indexRoutesBehaviors = require('./indexRoutesBehaviors');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  indexRoutesBehaviors.get.index.v2(req, res);
+/* GET home page.(Archived) */
+// router.get('/', function(req, res) {
+//   indexRoutesBehaviors.get.index.v2(req, res);
+// });
+
+router.get('/', function (req, res) {
+  res.render('index', {
+    facebookShare : {
+      pageUrl : 'http://jiaowojiejie.com',
+      pageTitle : '叫我姐姐',
+      pageDescription : '一款超简单信息量超大的实用趣味新闻游览器',
+      pageThumbnail : 'http://i.imgur.com/zyU0ChY.png'
+    }
+  });
 });
 
 router.post('/angularjs-order-test', function (req, res) {
